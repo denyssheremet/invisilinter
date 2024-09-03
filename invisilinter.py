4153d30c1836828660e264fb57b359d5a1e2b17f
Denys Sheremet <D.O.Sheremet@student.tudelft.nl>
2024-09-03 21:29:36 +0200

    the function

---
+++ invisilinter.py
@@ -0,0 +1,39 @@
+import ast
+
+evildict = {"a": "а",
+"c": "с",
+"d": "ԁ",
+"e": "е",
+"i": "і",
+"j": "ј",
+"n": "ո",
+"o": "о",
+"p": "р",
+"v": "ν",
+"x": "х",
+"y": "у"}
+
+def fuck_up_name(vname):
+    new_name = ""
+    added = False
+    for letter in vname:
+        if (not added) and (letter in evildict.keys()):
+            new_name += evildict[letter]
+            added=True
+        else:
+            new_name += letter
+        
+    return new_name
+
+
+class toLower(ast.NodeTransformer):
+    def visit_alias(self, node):
+        return ast.alias(**{**node.__dict__, 'asname':fuck_up_name(node.asname)})
+
+    def visit_Name(self, node):
+        return ast.Name(**{**node.__dict__, 'id':fuck_up_name(node.id)})
+
+
+def wreak_havoc(src_code):
+    '''Call this function! Pass the code as parameter.'''
+    return ast.unparse(toLower().visit(ast.parse(src_code)))
